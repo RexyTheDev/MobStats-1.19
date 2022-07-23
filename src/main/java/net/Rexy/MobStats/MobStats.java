7ffd8b3fba29851667b88f45fc52fe914e928859
Rexy <Smeltables@gmail.com>
2022-07-23 16:32:59 -0400

    Code

---
+++ src/main/java/net/Rexy/MobStats/MobStats.java
@@ -0,0 +1,14 @@
+package net.Rexy.MobStats;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MobStats implements ModInitializer {
+	public static final String MOD_ID = "MobStata";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/Rexy/MobStats/MobStatsClient.java
@@ -0,0 +1,10 @@
+package net.Rexy.MobStats;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MobStatsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
